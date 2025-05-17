8229145c46d385ce33f8127d3c8bdfcbab2d80dc
Eulier González <eulier1@gmail.com>
2025-05-17 14:44:05 -0300

    add routing for signup and order page

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,8 @@
   <div class="min-h-screen flex flex-col items-center justify-center bg-gray-50">
     <h1 class="text-2xl font-bold mb-4">Wireframes</h1>
     <UButton class="mt-8" @click="goToRestaurantMenu">Menu Dishes List</UButton>
+    <UButton class="mt-8" @click="goToSignUp">Sign up Page</UButton>
+    <UButton class="mt-8" @click="goToOrder">Order Page</UButton>
   </div>
 </template>
 
@@ -13,5 +15,13 @@ const router = useRouter()
 
 const goToRestaurantMenu = () => {
   router.push('/menu')
+}
+
+const goToOrder = () => {
+  router.push('/order')
+}
+
+const goToSignUp = () => {
+  router.push('/signup')
 }
 </script>
